18e5585e3ae7f981f0d6aeec285a6d54e7367edd
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-11-07 17:31:40 +0330

    refactor: updating the DatePicker into a class to improve modularity

--- main.js
+++ main.js
@@ -1,135 +1,142 @@
-const datePicker = document.getElementById("date-picker");
-const dateInput = document.getElementById("date-input");
-const yearInput = document.getElementById("year-input")
-const monthInput = document.getElementById("month-input")
-const cancelBtn = document.querySelector(".cancel")
-const applyBtn = document.querySelector(".apply")
-const dates = document.querySelector(".dates")
-const nextBtn = document.getElementById("next-btn")
-const prevBtn = document.getElementById("prev-btn")
-
-let selectedDate = new Date()
-let year = selectedDate.getFullYear()
-let month = selectedDate.getMonth()
-
-dateInput.addEventListener("click", () => {
-    datePicker.hidden = false;
-})
-
-cancelBtn.addEventListener("click", () => {
-    datePicker.hidden = true;
-})
-
-applyBtn.addEventListener("click", () => {
-    // the output is an object and needs to be formatted
-    dateInput.value = selectedDate.toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-    });
-
-    datePicker.hidden = true;
-})
-
-
-nextBtn.addEventListener("click", () => {
-    if (month === 11) year++;
-    month = (month + 1) % 12;
-    displayDates();
-})
-
-prevBtn.addEventListener("click", () => {
-    if (month === 0) year--;
-    month = (month + 1 + 12) % 12;
-    displayDates();
-})
-
-monthInput.addEventListener("change", () => {
-    month = monthInput.selectedIndex;
-    displayDates();
-})
-
-yearInput.addEventListener("change", () => {
-    year = yearInput.value;
-    displayDates();
-})
-
-function updateYearMonth() {
-    // it's a select input and we can change the index of the option
-    monthInput.selectedIndex = month;
-
-    yearInput.value = year;
-}
-
-
-function handleDateClick(e) {
-    const button = e.target
-
-    const selected = dates.querySelector(".selected")
-    selected && selected.classList.remove("selected")
-
-    button.classList.add("selected")
-
-    selectedDate = new Date(year, month, parseInt(button.textContent))
-}
-
-
-function displayDates() {
-    // update year and month with each rerender caused by user input
-    updateYearMonth()
-
-    // clear the dates
-    dates.innerHTML = ""
-
-    // display the last week of previous month
-    // day and months are zero indexed
-    // by passing 0 as the last argument of the Date() we are targeting the last day of the prev month
-    const lastDayOfPrevMonth = new Date(year, month, 0)
-
-
-    for (let i = 0; i <= lastDayOfPrevMonth.getDay(); i++) {
-        // i = 0 => Sunday
-        // (last day of prev month - what day of the week it was) + i to populate the empty days of the calendar
-        const text = (lastDayOfPrevMonth.getDate() - lastDayOfPrevMonth.getDay() + i);
-        const button = createDatesButton(text, true);
-        dates.appendChild(button);
-    }
-
-    // display the current month
-    const lastDayOfMonth = new Date(year, month + 1, 0)
-    for (let i = 1; i <= lastDayOfMonth.getDate(); i++) {
-        // i = 1 => cause of days being zero indexed
-
-        const button = createDatesButton(i, false)
-        button.addEventListener("click", handleDateClick)
-        dates.appendChild(button)
+class DatePicker {
+    constructor(container) {
+        this.container = container;
+        this.dateInput = container.querySelector(".date-input");
+        this.datePicker = container.querySelector(".date-picker");
+        this.selectedDate = new Date();
+        this.year = this.selectedDate.getFullYear();
+        this.month = this.selectedDate.getMonth();
+        this.dates = container.querySelector(".dates");
+        this.monthInput = container.querySelector(".month-input");
+        this.yearInput = container.querySelector(".year-input");
+        this.cancelBtn = container.querySelector(".cancel");
+        this.applyBtn = container.querySelector(".apply");
+        this.nextBtn = container.querySelector("#next-btn");
+        this.prevBtn = container.querySelector("#prev-btn");
+        this.init();
     }
 
 
-    // display the first week of next month
-    const firstDateOfNextMonth = new Date(year, month + 1, 1)
-    for (let i = firstDateOfNextMonth.getDay(); i < 7; i++) {
-        const text = (firstDateOfNextMonth.getDate() - firstDateOfNextMonth.getDay() + i);
-        const button = createDatesButton(text, true)
-        dates.appendChild(button)
+    init() {
+        this.displayDates();
+        this.addListeners();
     }
 
+
+    displayDates() {
+        // update year and month with each rerender caused by user input
+        this.updateYearMonth();
+
+        // clear the dates
+        this.dates.innerHTML = "";
+
+        // display the last week of previous month
+        // day and months are zero indexed
+        // by passing 0 as the last argument of the Date() we are targeting the last day of the prev month
+        const lastDayOfPrevMonth = new Date(this.year, this.month, 0);
+
+
+        for (let i = 0; i <= lastDayOfPrevMonth.getDay(); i++) {
+            // i = 0 => Sunday
+            // (last day of prev month - what day of the week it was) + i to populate the empty days of the calendar
+            const text = lastDayOfPrevMonth.getDate() - lastDayOfPrevMonth.getDay() + i;
+            const button = this.createDatesButton(text, true);
+            this.dates.appendChild(button)
+        }
+
+        // display the current month
+        const lastDayOfMonth = new Date(this.year, this.month + 1, 0);
+        for (let i = 1; i <= lastDayOfMonth.getDate(); i++) {
+            // i = 1 => cause of days being zero indexed
+            const button = this.createDatesButton(i, false)
+            button.addEventListener("click", this.handleDateClick)
+            this.dates.appendChild(button);
+        }
+
+        // display the first week of next month
+        const firstDateOfNextMonth = new Date(this.year, this.month + 1, 1);
+        for (let i = firstDateOfNextMonth.getDay(); i < 7; i++) {
+            const text = firstDateOfNextMonth.getDate() - firstDateOfNextMonth.getDay() + i;
+            const button = this.createDatesButton(text, true)
+            this.dates.appendChild(button);
+        }
+    }
+
+    addListeners() {
+        this.dateInput.addEventListener("click", () => this.datePicker.hidden = false);
+        this.cancelBtn.addEventListener("click", () => this.datePicker.hidden = true);
+        this.applyBtn.addEventListener("click", () => this.applyDate());
+        this.nextBtn.addEventListener("click", () => this.nextMonth());
+        this.prevBtn.addEventListener("click", () => this.prevMonth());
+        this.monthInput.addEventListener("change", () => {
+            this.month = this.monthInput.selectedIndex;
+            this.displayDates();
+        });
+        this.yearInput.addEventListener("change", () => {
+            this.year = this.yearInput.value;
+            this.displayDates();
+        });
+    }
+
+
+    applyDate() {
+        // the output is an object and needs to be formatted
+        this.dateInput.value = this.selectedDate.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+        });
+
+        this.datePicker.hidden = true;
+    }
+
+    nextMonth() {
+        if (this.month === 11) this.year++;
+        this.month = (this.month + 1) % 12;
+        this.displayDates();
+    }
+
+    prevMonth() {
+        if (this.month === 0) this.year--;
+        this.month = (this.month - 1 + 12) % 12;
+        this.displayDates();
+    }
+
+    updateYearMonth() {
+        // it's a select input and we can change the index of the option
+        this.monthInput.selectedIndex = this.month;
+
+        this.yearInput.value = this.year;
+    }
+
+    // HAS TO BE AN ARROW FUNCTION TO INHERIT "THIS" SCOPE FROM IT'S PARENT. OTHERWISE WE WOULD GET UNDEFINED ERROR FOR RETRIEVING "SELECTED" ELEMENT.
+    // WE CAN KEEP THIS LIKE NORMAL METHOD AND USE IT LIKE button.addEventListener("click", (e) => this.handleDateClick(e)); WHEN ADDING EVENTLISTENER TO THE BUTTON ELEMENT
+    handleDateClick = (e) => {
+        const button = e.target;
+        const selected = this.dates.querySelector(".selected");
+
+        selected && selected.classList.remove("selected");
+        button.classList.add("selected");
+        this.selectedDate = new Date(this.year, this.month, parseInt(button.textContent));
+    }
+
+    createDatesButton(text, isDisabled = false) {
+        const currentDate = new Date();
+        const isToday =
+            currentDate.getDate() === text &&
+            currentDate.getFullYear() === this.year &&
+            currentDate.getMonth() === this.month;
+
+        const li = document.createElement("li");
+        const button = document.createElement("button");
+        button.textContent = text;
+        button.disabled = isDisabled;
+        button.classList.toggle("today", isToday);
+
+        li.appendChild(button);
+        return li;
+    }
 }
 
-function createDatesButton(text, isDisabled = false) {
-    const currentDate = new Date();
-
-    const isToday = currentDate.getDate() === text && currentDate.getFullYear() === year && currentDate.getMonth() === month;
-
-    const li = document.createElement("li");
-    const button = document.createElement("button");
-    button.textContent = text;
-    button.disabled = isDisabled;
-    button.classList.toggle("today", isToday);
-
-    li.appendChild(button);
-    return li;
-}
-
-
-displayDates()
+document.querySelectorAll('.date-picker-container')
+    .forEach(container => new DatePicker(container));
